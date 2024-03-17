ac4c112be18c264529b1466cfd99a292f0bf2923
G_EST <gest60599@gmail.com>
2024-03-17 12:22:49 +1000

    d23register

--- d18/users/forms.py
+++ d18/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, forms
 from .models import User
 
 
@@ -6,3 +6,24 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' :'form-control py-4',
+        'placeholer':'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Введите фамилию'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'from-control py-4',
+        'placeholer': 'Введите email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Повторите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username','password1','password2','email']
