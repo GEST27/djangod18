683ef44670f845bbf0581e911d651f6d1d4ecbba
G_EST <gest60599@gmail.com>
2024-04-13 15:35:04 +1000

    d26 all

--- d18/users/views.py
+++ d18/users/views.py
@@ -43,12 +43,22 @@ def profile(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))
-        else:print(form.errors)
+        else:
+            print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
+    baskets = Basket.objects.filter(user = request.user)
+    total_sum = 0
+    total_quantity = 0
+    for i in baskets:
+        total_sum += i.sum()
+        total_quantity += i.quantity
+
     context = {'Title':'Профиль',
                'form': form,
-               'baskets': Basket.objects.filter(user = request.user)}
+               'baskets': baskets,
+               'total_sum':total_sum,
+               'total_quantity':total_quantity}
     return render(request, 'users/profile.html', context = context)
 
 def logout(request):
