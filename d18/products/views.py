a11e25852208569c04c9198d93d3a08e49a29481
G_EST <gest60599@gmail.com>
2024-04-14 17:37:20 +1000

    d27deco

--- d18/products/views.py
+++ d18/products/views.py
@@ -3,19 +3,29 @@ from django.http import HttpResponseRedirect
 from .models import Product, ProductCategory, Basket
 from users.models import User
 from django.contrib.auth.decorators import login_required
+
+
 def index(request):
     context = {
         'title': 'Магазик'
     }
     return render(request, 'index.html', context = context)
 
-def products(request):
+def products(request, category_id = None):
+    if category_id :
+        category = ProductCategory.objects.get(id = category_id)
+        products = Product.objects.filter(category = category)
+    else:
+        products = Product.objects.all()
+
     context = {
-        'title':'Магазик-каталог',
-        'products' : Product.objects.all(),
+        'title': 'Магазик-каталог',
         'categories': ProductCategory.objects.all(),
+        'products':products,
     }
     return render(request, 'products.html', context = context)
+
+
 
 @login_required
 def basket_add(request, product_id:id):
